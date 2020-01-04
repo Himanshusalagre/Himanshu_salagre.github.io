6c8e3771fd56de53f07e524853204a2394eba48a
Himanshu Salagre <33714300+Himanshusalagre@users.noreply.github.com>
2020-01-04 13:36:14 +0530

    Update about.php

--- about.php
+++ about.php
@@ -26,22 +26,22 @@
       </div>
       <ul class="menu-nav">
         <li class="nav-item">
-          <a href="index.html" class="nav-link">
+          <a href="index.php" class="nav-link">
             Home
           </a>
         </li>
         <li class="nav-item current">
-          <a href="about.html" class="nav-link">
+          <a href="about.php" class="nav-link">
             About Me
           </a>
         </li>
         <li class="nav-item">
-          <a href="work.html" class="nav-link">
+          <a href="work.php" class="nav-link">
             My Work
           </a>
         </li>
         <li class="nav-item">
-          <a href="contact.html" class="nav-link">
+          <a href="contact.php" class="nav-link">
             Contact Me
           </a>
         </li>
